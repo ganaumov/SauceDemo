fa1bdffa93b17e453652d53adbb25c34fff689f3
naumov vladimir <xahdrift@gmail.com>
2025-06-10 22:50:03 +0300

    added secret fixed YML

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -25,8 +25,8 @@ public class BaseTest {
     CartPage cartPage;
     CheckOutPage checkOutPage;
 
-    String user = System.getProperty("user");
-    String password = System.getProperty("password");
+    String user = System.getProperty("user",PropertyReader.getProperty("user"));
+    String password = System.getProperty("password",PropertyReader.getProperty("password"));
 
     @Parameters({"browser"})
     @BeforeMethod
